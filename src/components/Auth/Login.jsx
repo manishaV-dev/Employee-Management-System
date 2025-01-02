fb5699d558a2f6da5353f3ce544c5d32a59abe08
Manisha varma <manishavkapex1@gmail.com>
2025-01-02 22:10:47 +0530

    add two way binding

--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -1,19 +1,45 @@
-import React from 'react'
+import React, { useState } from "react";
 
 function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    console.log("Form Submitted");
+  };
+
   return (
     <>
-    <div className="flex items-center justify-center h-screen w-screen">
-        <div className="border-2 border-purple-400 px-10 py-20">
-            <form className='flex flex-col items-center justify-center'>
-                <input className='text-white border-2 border-purple-400 px-5 py-2 rounded-full text-sm outline-none bg-transparent placeholder:text-gray-400' type="email" placeholder='Enter your email' />
-                <input className='text-white border-2 border-purple-400 px-5 py-2 rounded-full text-sm outline-none bg-transparent placeholder:text-gray-400 mt-3' type="password" placeholder='Enter your password' />
-                <button className='text-white border-none px-8 py-2 rounded-full text-sm outline-none bg-purple-600 placeholder:text-gray-400 mt-3'>Login</button>
-            </form>
+      <div className="flex items-center justify-center h-screen w-screen">
+        <div className="border-2 border-purple-400 px-10 py-20 rounded-lg">
+          <form
+            onSubmit={submitHandler}
+            className="flex flex-col items-center justify-center"
+          >
+            <input
+              className="text-white border-2 border-purple-400 px-5 py-2 rounded-full text-sm outline-none bg-transparent placeholder:text-gray-400"
+              type="email"
+              placeholder="Enter your email"
+              required
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <input
+              className="text-white border-2 border-purple-400 px-5 py-2 rounded-full text-sm outline-none bg-transparent placeholder:text-gray-400 mt-3"
+              type="password"
+              required
+              placeholder="Enter your password"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <button className="text-white border-none px-8 py-2 rounded-full text-sm outline-none bg-purple-600 placeholder:text-gray-400 mt-3">
+              Login
+            </button>
+          </form>
         </div>
-    </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default Login
+export default Login;
