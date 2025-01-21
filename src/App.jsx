8c4b8ce08f0e37771c85c3d6ec4ec38ea962cd63
Manisha varma <manishavkapex1@gmail.com>
2025-01-21 12:22:40 +0530

    pass data from child to parent

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,31 @@ import "./App.css";
 import AdminDashboard from "./components/Dashboard/AdminDashboard";
 import Login from "./components/Auth/Login";
 import EmployeeDashboard from "./components/Dashboard/EmployeeDashboard";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { getLocalStorage, setLocalStorage } from "./utils/localStorage";
 
 function App() {
+  const [user, setUser] = useState(null);
 
+  // useEffect(() => {
+  //   // setLocalStorage()
+  //   getLocalStorage()
+  // }, [])
 
-  useEffect(() => {
-    // setLocalStorage()
-    getLocalStorage()
-  }, [])
+  const handleLogin = (email, password) => {
+    if (email == "admin@me.com" && password == "123") {
+      console.log("This is Admin");
+    } else if (email == "user@me.com" && password == "123") {
+      console.log("This is user");
+    } else {
+      alert("Invalid Credentials");
+    }
+  };
 
   return (
     <>
-      <Login />
+      {!user ? <Login handleLogin={handleLogin} /> : ""}
+
       {/* <EmployeeDashboard /> */}
       {/* <AdminDashboard /> */}
     </>
--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -1,12 +1,13 @@
 import React, { useState } from "react";
 
-function Login() {
+function Login({ handleLogin }) {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
   const submitHandler = (e) => {
     e.preventDefault();
     // console.log("Form Submitted", email, password);
+    handleLogin(email, password);
     setEmail("");
     setPassword("");
   };
