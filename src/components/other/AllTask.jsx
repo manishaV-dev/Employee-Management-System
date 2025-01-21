5f9f4e54a105d21cb0efa9c05795347bc2ad486d
Manisha varma <manishavkapex1@gmail.com>
2025-01-21 18:36:18 +0530

    display employee existing tasks in Admin

--- src/components/other/AllTask.jsx
+++ src/components/other/AllTask.jsx
@@ -1,33 +1,46 @@
-import React from "react";
+import React, { useContext } from "react";
+import { AuthContext } from "../../context/AuthProvider";
 
 function AllTask() {
+  const authData = useContext(AuthContext);
+  // console.log("Admin All Task", authData); // {employeeData: Array(5), adminData: Array(1)
+
   return (
     <>
-      <div id="alltask" className="bg-[#1c1c1c] p-5 mt-5 rounded h-40 overflow-auto">
-        <div className="bg-red-400 py-2 mb-2 px-4 flex justify-between rounded">  
-          <h2>Manisha</h2>
-          <h3>Make a UI Design</h3>
-          <h2>Status</h2>
+      <div
+        id="alltask"
+        className="bg-[#1c1c1c] p-5 mt-5 rounded h-40 overflow-auto"
+      >
+        <div className="bg-red-400 py-2 mb-2 px-4 flex justify-between rounded">
+          <h2 className="font-medium w-1/5">Employee</h2>
+          <h3 className="font-medium w-1/5">New Task</h3>
+          <h2 className="font-medium w-1/5">Active Task</h2>
+          <h2 className="font-medium w-1/5">Completed</h2>
+          <h2 className="font-medium w-1/5">Failed </h2>
         </div>
 
-        <div className="bg-blue-400 py-2 mb-2 px-4 flex justify-between rounded">  
-          <h2>Manisha</h2>
-          <h3>Make a UI Design</h3>
-          <h2>Status</h2>
-        </div>
-
-
-        <div className="bg-orange-400 py-2 mb-2 px-4 flex justify-between rounded">  
-          <h2>Manisha</h2>
-          <h3>Make a UI Design</h3>
-          <h2>Status</h2>
-        </div>
-
-        <div className="bg-green-400 py-2 mb-2 px-4 flex justify-between rounded">  
-          <h2>Manisha</h2>
-          <h3>Make a UI Design</h3>
-          <h2>Status</h2>
-        </div>
+        {authData.employeeData.map((elem, index) => {
+          return (
+            <div
+              className="border-2 border-emerald-500 py-2 mb-2 px-4 flex justify-between rounded"
+              key={index}
+            >
+              <h2 className="w-1/5">{elem.firstName}</h2>
+              <h3 className="text-blue-500 w-1/5 font-medium">
+                {elem.taskCounts.newTask}
+              </h3>
+              <h2 className="text-green-500 w-1/5 font-medium">
+                {elem.taskCounts.active}
+              </h2>
+              <h2 className="text-yellow-500 w-1/5 font-medium">
+                {elem.taskCounts.completed}
+              </h2>
+              <h2 className="text-red-500 w-1/5 font-medium">
+                {elem.taskCounts.failed}
+              </h2>
+            </div>
+          );
+        })}
       </div>
     </>
   );
